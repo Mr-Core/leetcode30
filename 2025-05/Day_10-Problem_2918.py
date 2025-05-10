b7c8c146e0665630c7d40f72f44714d4adcde761
Mr-Core <ivan.stanec@gmail.com>
2025-05-10 12:05:09 +0200

    Solved another challenge

---
+++ 2025-05/Day_10-Problem_2918.py
@@ -0,0 +1,88 @@
+"""
+2918. Minimum Equal Sum of Two Arrays After Replacing Zeros (Medium)
+
+You are given two arrays nums1 and nums2 consisting of positive integers.
+You have to replace all the 0's in both arrays with strictly positive integers such that the sum of elements of both arrays becomes equal.
+Return the minimum equal sum you can obtain, or -1 if it is impossible.
+
+
+Example 1:
+
+Input: nums1 = [3,2,0,1,0], nums2 = [6,5,0]
+Output: 12
+Explanation: We can replace 0's in the following way:
+- Replace the two 0's in nums1 with the values 2 and 4. The resulting array is nums1 = [3,2,2,1,4].
+- Replace the 0 in nums2 with the value 1. The resulting array is nums2 = [6,5,1].
+Both arrays have an equal sum of 12. It can be shown that it is the minimum sum we can obtain.
+
+
+Example 2:
+
+Input: nums1 = [2,0,2,0], nums2 = [1,4]
+Output: -1
+Explanation: It is impossible to make the sum of both arrays equal.
+
+
+Constraints:
+    1 <= nums1.length, nums2.length <= 10^5
+    0 <= nums1[i], nums2[i] <= 10^6
+
+
+Hint 1
+Consider we replace all the 0s with 1s on both arrays, the answer will be -1 if there was no 0 in the array with the smaller sum of elements.
+
+Hint 2
+Otherwise, how can you update the value of exactly one of these 1s to make the sum of the two arrays equal?
+"""
+
+# --------
+# Testing
+# --------
+
+# from typing import List  # Optional
+#
+#
+# def minSum(nums1: List[int], nums2: List[int]) -> int:
+#     sum1 = sum(nums1)
+#     sum2 = sum(nums2)
+#
+#     z1 = nums1.count(0)
+#     z2 = nums2.count(0)
+#
+#     min_sum1 = sum1 + z1 * 1
+#     min_sum2 = sum2 + z2 * 1
+#
+#     if (min_sum1 < min_sum2 and z1 == 0) or (min_sum2 < min_sum1 and z2 == 0):
+#         return -1
+#
+#     return max(min_sum1, min_sum2)
+#
+#
+# nums1 = [3, 2, 0, 1, 0]
+# nums2 = [6, 5, 0]
+#
+# print(minSum(nums1, nums2))
+
+
+# ---------------------------------------
+# Solution (848ms runtime, beats 64.99%)
+# ---------------------------------------
+
+from typing import List  # Optional
+
+
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        sum1 = sum(nums1)
+        sum2 = sum(nums2)
+
+        z1 = nums1.count(0)
+        z2 = nums2.count(0)
+
+        min_sum1 = sum1 + z1 * 1
+        min_sum2 = sum2 + z2 * 1
+
+        if (min_sum1 < min_sum2 and z1 == 0) or (min_sum2 < min_sum1 and z2 == 0):
+            return -1
+
+        return max(min_sum1, min_sum2)
